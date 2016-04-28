40bd65290b9aac2a757e1e3ea11ac4c6e9af2d17
zwb <zwbFight@163.com>
2016-04-28 19:09:11 +0800

    scrollerview 嵌套listview

--- StudyDemo/toucheventdemo1/src/main/java/com/example/toucheventdemo1/MainActivity.java
+++ StudyDemo/toucheventdemo1/src/main/java/com/example/toucheventdemo1/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,26 +25,35 @@ public class MainActivity extends Activity {
 
     @InjectView(R.id.listView)
     ListView listView;
+    @InjectView(R.id.tv)
+    TextView tv;
 
     private List<String> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
-        for (int i =0;i<100;i++){
-            list.add("test----"+i);
+        for (int i = 0; i < 100; i++) {
+            list.add("test----" + i);
         }
-        MyAdapter adapter =  new MyAdapter(list);
+        MyAdapter adapter = new MyAdapter(list);
         listView.setAdapter(adapter);
+//        tv = new TextView(this);
+//        tv.setText("sadfssssssssssssssss");
+//        listView.addHeaderView(tv);
+        listView.setEmptyView(tv);
+//        setListViewHeightBasedOnChildren(listView);
     }
 
-    public class MyAdapter extends BaseAdapter{
+    public class MyAdapter extends BaseAdapter {
         List<String> list;
-        MyAdapter(List<String> list){
+
+        MyAdapter(List<String> list) {
             this.list = list;
-            Log.i("info",list.size()+"");
+            Log.i("info", list.size() + "");
         }
 
         @Override
@@ -64,14 +74,57 @@ public class MainActivity extends Activity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             TextView textView;
-            if(view == null){
-                view = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item,viewGroup,false);
-                textView = (TextView)view.findViewById(R.id.text);
-            }else{
-                textView = (TextView)view.findViewById(R.id.text);
+            if (view == null) {
+                Log.i("info", "getview---=====" + i);
+                view = LayoutInflater.from(MainActivity.this).inflate(R.layout.list_item, viewGroup, false);
+                textView = (TextView) view.findViewById(R.id.text);
+            } else {
+                textView = (TextView) view.findViewById(R.id.text);
             }
             textView.setText(list.get(i));
             return view;
         }
+    }
+
+    public void setListViewHeightBasedOnChildren(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+
+        if (listAdapter == null) {
+
+            return;
+
+        }
+
+        int totalHeight = 0;
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+
+            View listItem = listAdapter.getView(i, null, listView);
+
+            listItem.measure(0, 0);
+
+            totalHeight += listItem.getMeasuredHeight();
+
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+        params.height = totalHeight
+
+                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+
+        // params.height += 5;// if without this statement,the listview will be
+
+        // a
+
+        // little short
+
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+
+        // params.height最后得到整个ListView完整显示需要的高度
+
+        listView.setLayoutParams(params);
+
     }
 }
--- StudyDemo/toucheventdemo1/src/main/java/view/MyScrollerView.java
+++ StudyDemo/toucheventdemo1/src/main/java/view/MyScrollerView.java
@@ -17,31 +17,10 @@ public class MyScrollerView extends ScrollView {
     }
     public MyScrollerView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
     }
 
     public MyScrollerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context,attrs,defStyleAttr);
-
-    }
-
-    //事件分发
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        return super.dispatchKeyEvent(event);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if(ev.getAction() == MotionEvent.ACTION_MOVE){
-            return true;
-        }
-        return super.onInterceptTouchEvent(ev);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        return true;
     }
 
 }
