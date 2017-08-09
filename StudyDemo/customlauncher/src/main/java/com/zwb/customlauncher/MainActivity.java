8dc092bd0fbb88a84d6eb90d70fc87f8151dcc25
zwb1992 <zwbfight@163.com>
2017-08-09 22:51:24 +0800

    完善布局

--- StudyDemo/customlauncher/src/main/java/com/zwb/customlauncher/MainActivity.java
+++ StudyDemo/customlauncher/src/main/java/com/zwb/customlauncher/MainActivity.java
@@ -1,30 +1,55 @@
 package com.zwb.customlauncher;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout ll_info;
-
+    private RelativeLayout rl_info;
+    private Animation animation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ll_info = (LinearLayout) findViewById(R.id.ll_info);
-        ll_info.setOnClickListener(new View.OnClickListener() {
+        animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.left_in);
+        rl_info = (RelativeLayout) findViewById(R.id.rl_info);
+        rl_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-//                startActivity(intent);
-                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.left_in);
-                ll_info.startAnimation(animation);
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivity(intent);
             }
         });
+        handler.postDelayed(getDataRunnable,5000);
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            rl_info.startAnimation(animation);
+        }
+    };
+
+    private Runnable getDataRunnable = new Runnable() {
+        @Override
+        public void run() {
+            handler.sendEmptyMessage(1);
+            handler.postDelayed(this,5000);
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(getDataRunnable);
     }
 
     @Override
